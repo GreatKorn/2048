d101ab479495c426e95b2b7b07470a0520b06a23
Elisey.Ishim@yandex.ru <Elisey.ishim@yandex.ru>
2023-04-13 20:43:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+import { Grid } from "./grid.js";
+
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard);
+grid.getRamdomEmptyCell().linkTitle(new Tile(gameBoard));
